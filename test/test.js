bdf3ce5b6644e0767bd46cf74218d829881b56a5
Daniel Beardsley <daniel@ifixit.com>
2019-09-26 10:04:53 -0700

    Add tests for all of _js()

--- test/test.js
+++ test/test.js
@@ -4,9 +4,30 @@ const {_js, ___p} = require('../src/lib/translations.js');
 
 describe('_js', function() {
    it('should translate!', function() {
-      assert.equal('from', _js('from'));
       add('from', 'to');
       assert.equal('to', _js('from'));
+   });
+
+   it('should fallback to english!', function() {
+      assert.equal('not translated', _js('not translated'));
+   });
+
+   describe("parameters", function() {
+      it('should be replaced with args', function() {
+         assert.equal('before ME! after', _js('before %1 after', 'ME!'));
+      });
+      it('should be replaced when used more than once', function() {
+         assert.equal('me me me', _js('%1 %1 %1', 'me'));
+      });
+   });
+
+   it('should collapse spaces', function() {
+      add('many spaces', 'only one space');
+      assert.equal('only one space', _js('many     spaces'));
+   });
+
+   it('should collapse spaces when falling back', function() {
+      assert.equal('just one space', _js('just one    space'));
    });
 });
 

676b2d42991570d0336a82f220a311f2a0d03f38
Joshua Miles <josh.miles@dozuki.com>
2022-03-15 12:28:20 -0500

    Add types declaration file

---
+++ localize.d.ts
@@ -0,0 +1,8 @@
+export function _js(origString: string, ...replacements: string[]): string;
+export function ___p(
+   number: number,
+   singularString: string,
+   pluralString: string,
+   ...replacements: string[]
+): string;
+export function setTranslations(translations: any): void;
